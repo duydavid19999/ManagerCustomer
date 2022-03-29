60e92f3fc030d5b240157882b9f73e47ece83c3f
duydavid19999 <vucongduy1999@gmail.com>
2022-03-29 22:44:35 +0700

    test commit

--- ManagerCustomer/src/main/java/MavenInterface/DAOCustomer.java
+++ ManagerCustomer/src/main/java/MavenInterface/DAOCustomer.java
@@ -3,7 +3,7 @@ package MavenInterface;
 import MavenCls.Customer;
 
 import java.util.ArrayList;
-
+// dhdasdhahfskljasjfoisofoiosfosd
 public interface DAOCustomer {
     void inputCustomer(ArrayList<Customer> T);
     void deleteIndexCustomer(ArrayList<Customer> T,int vt);
